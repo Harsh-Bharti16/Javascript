725932f34691bbb3c68553be431b6083d69da956
Harsh-Bharti16 <114231941+Harsh-Bharti16@users.noreply.github.com>
2024-08-17 06:55:50 +0000

    dataype summery

---
+++ .devcontainer/javascript/datatypes.js
@@ -0,0 +1,35 @@
+// primitive data types 
+
+// string - call by value , number , boolean , null , undefined , symbol - to make value unique , BigInt
+
+//Reference - Non primitive , array , objects 
+
+// big int represnts with n at the end of the number
+
+// const bigNumber = 82539593859475498
+
+// objects are defined in the {} Brackets
+
+// let myObj{
+//     name: "Harsh",
+//     age: 22
+
+// }
+
+//non primitive datatypes - which data types use i.e. if objects used retursn objecct
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
