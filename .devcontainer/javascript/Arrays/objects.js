21d13a654d05452d32548aca8921fd9c2f331013
Harsh-Bharti16 <114231941+Harsh-Bharti16@users.noreply.github.com>
2024-08-31 07:22:13 +0000

    objects

--- .devcontainer/javascript/Arrays/objects.js
+++ .devcontainer/javascript/Arrays/objects.js
@@ -1,8 +1,15 @@
-const myObj = {
+// const myObj = {
 
-    name: "Harsh Bharti",
-    number:"7000634457",
-    email : "harsh@gmail.com"
+//     name: "Harsh Bharti",
+//     number:"7000634457",
+//     email : "harsh@gmail.com"
 
+// }
+// console.log(myObj); 
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+const obj3 = {...obj1,...obj2
 }
-console.log(myObj); 
+console.log(obj3);
